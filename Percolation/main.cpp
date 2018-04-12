ef4be7bce3c5f976ee66e4f8f581ae0516bf2feb
Crystal Xu <guaixiaoxu@outlook.com>
2018-04-12 22:49:00 +0800

    Create main.cpp

---
+++ Percolation/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  Union Find
+//
+//  Created by Crystal Xu on 02/04/2018.
+//  Copyright © 2018 Crystal Xu. All rights reserved.
+//
+
+#include <iostream>
+#include "percolation.hpp"
+
+int main(int argc, const char * argv[]) {
+    Percolation *perco = new Percolation(200);
+    perco->Construct();
+    std::cout << perco->counts() << std::endl;
+    UF *uf = new UF(10);
+    uf->w_union(0, 1);
+    uf->w_union(2, 1);
+    uf->w_union(3, 1);
+    uf->w_union(4, 5);
+    int max1 = uf->find(1);
+    int max2 = uf->find(5);
+    std::cout << max1 << " " << max2 << std::endl;
+    return 0;
+}
