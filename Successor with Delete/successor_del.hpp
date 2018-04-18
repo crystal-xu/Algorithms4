6e523c954d5cb0e0742f1693ea01b144b1c10e50
Crystal Xu <guaixiaoxu@outlook.com>
2018-04-18 08:51:21 +0800

    Update successor_del.hpp

--- Successor with Delete/successor_del.hpp
+++ Successor with Delete/successor_del.hpp
@@ -6,6 +6,13 @@
 //  Copyright © 2018 Crystal Xu. All rights reserved.
 //
 
+/*
+Given a set of n integers S={0,1,...,n−1} and a sequence of requests of the following form:
+• Remove x from S
+• Find the successor of x: the smallest y in S such that y≥x.
+design a data type so that all operations (except construction) take logarithmic time or better in the worst case.
+*/
+
 #ifndef successor_del_hpp
 #define successor_del_hpp
 
